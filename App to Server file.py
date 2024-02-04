55a4b8ad1e570121975c63ad404c26e89ab5bb4f
Jacob Brock <113866775+jhbrock21@users.noreply.github.com>
2024-02-04 11:24:43 -0500

    Database Connection Code

---
+++ App to Server file.py
@@ -0,0 +1,35 @@
+#Install required packages for both Django and PostgreSQL adapter
+pip install Django psycopg2-binary
+
+#Configure Database settings in the settings.py file of the Django Project
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'databasename',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+#Run migrations
+python manage.py makemigrations
+python manage.py migrate
+
+#Define models(tables in database) in Django
+from django.db import models
+
+class ModelName(models.Model):
+    Username = models.CharField(max_length=255)
+    Password = models.IntegerField()
+    #Add more if needed
+
+#Use Django ORM to interact with Database
+from .models import ModelName
+data = ModelName.objects.all()
+
+new_record = ModelName(Username='username', Password=1)
+new_record.save()
+
+python manage.py runserver
