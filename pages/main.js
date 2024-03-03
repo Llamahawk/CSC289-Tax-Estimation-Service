37517fd15e35d6812cbde9680715a1faa867af8b
ChillWampa <mtartjokull@gmail.com>
2024-03-03 11:02:27 -0500

    javascript and style

--- pages/main.js
+++ pages/main.js
@@ -1,1 +1,11 @@
-onclick="window.open"
+var display = true;
+var username = "User";
+
+function loggedIn()
+{
+    if (display == true)
+    {
+        location.href = "main.html";
+        document.getElementById("bannerButtons").innerHTML = username;
+    }
+}
