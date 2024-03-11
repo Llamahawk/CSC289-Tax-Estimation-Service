96faf6dff046baef707ee54e6e1194db5c865c6c
kristinamarkina <kr.markina@gmail.com>
2024-03-10 20:13:34 -0400

    Fixes login logic too

--- calculator_project/calculator_app/views.py
+++ calculator_project/calculator_app/views.py
@@ -1,6 +1,7 @@
 import json
 
 import bcrypt
+import base64
 from django.shortcuts import render, redirect
 from django.views import View
 
@@ -28,8 +29,9 @@ class LoginView(View):
         return render(request, self.template_name)
 
     def post(self, request):
-        email = request.POST.get('email')
+        email = request.POST.get('email_address')
         password = request.POST.get('password')
+        logger.debug(f"I got email and password {email} {password}")
 
         # Load user data from JSON file
         with open(self.user_dictionary, 'r') as file:
@@ -37,18 +39,26 @@ class LoginView(View):
 
         # Check if user with given email exists
         user = next((user for user in user_data_list if user['email_address'] == email), {})
+        logger.debug(f"Did I find the user? {user}")
 
         try:
-            if user and bcrypt.checkpw(password.encode('utf-8'), user['hashed_password'].encode('utf-8')):
-                # Authentication successful, redirect to dashboard or another page
-                return redirect('dashboard')
-            else:
+            if not user:
                 # Authentication failed, handle accordingly
-                raise ValueError('Invalid credentials')
+                logger.debug('User not found. Go to sign up and create a new user')
+                raise ValueError('No user found')
+            else:
+                logger.debug("User found. Let's check the password")
+                if bcrypt.checkpw(password.encode('utf-8'), base64.b64decode(user["hashed_password"].encode('utf-8'))):
+                    # Authentication successful, redirect to dashboard or another page
+                    logger.debug("Password is correct")
+                    return redirect('../dashboard')
+                else:
+                    logger.debug('Invalid credentials for user: %s', user['email_address'])
+                    logger.debug(f"Entered password: {password}, Hashed password: {user['hashed_password']}")
+                    raise ValueError('Invalid credentials')
         except ValueError as e:
             # Handle authentication failure gracefully
             return render(request, self.template_name, {'error_message': str(e)})
-
 
 class SignUpView(View):
     template_name = 'calculator_app/signup.html'
