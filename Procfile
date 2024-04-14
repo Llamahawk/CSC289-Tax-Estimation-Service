206bc72e94b0629366b5ec102607853d9dfc1623
Ian <98511451+Llamahawk@users.noreply.github.com>
2024-04-14 11:16:52 -0400

    fix style, change settings for deployment

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 release: python calculator_project/manage.py migrate
-release: python calculator_project/manage.py runserver
 web: gunicorn calculator_project.wsgi
--- calculator_project/calculator_project/settings.py
+++ calculator_project/calculator_project/settings.py
@@ -27,7 +27,7 @@ LOGGING = {
     },
 }
 
-ALLOWED_HOSTS = ['https://tax-estimation-service-0833bdfda483.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
